97a0a8ac2fb47a3a6ec1b4cb5d1608d07bfa4164
kandaness <kandaness@163.com>
2017-12-31 17:15:12 +0800

    almost complete

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -56,11 +56,98 @@
     },
     computed:{
       shortcut(){
-        this._getHotKey()
+        return this.hotkey.concat(this.searchHistory)
       }
+    },
+    created(){
+      this._getHotKey()
+    },
+    methods:{
+      handlePlaylist(playlist){
+        const bottom=playlist.length>0?'60px':''
+        this.$refs.searchResult.style.bottom=bottom
+        this.$refs.suggest.refresh()
+        this.$refs.shortcutWrapper.style.bottom=bottom
+        this.$refs.shortcut.refresh()
+      },
+      showConfirm(){
+        this.$refs.confirm.show()
+      },
+      _getHotKey(){
+        getHotKey().then((res)=>{
+          this.hotkey=res.data.hotkey.slice(0,10)
+        })
+      },
+      ...mapActions([
+        'clearSearchHistory'
+      ])
+    },
+    watch(){
+      query(newQuery){
+        if(!newQuery){
+          setTimeout(()=>{
+            this.$refs.shortcut.refresh()
+          },20)
+        }
+      }
+    },
+    components:{
+      SearchBox,
+      SearchList,
+      Scroll,
+      Confirm,
+      Suggest
     }
   }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
 
+  .search
+    .search-box-wrapper
+      margin: 20px
+    .shortcut-wrapper
+      position: fixed
+      top: 178px
+      bottom: 0
+      width: 100%
+      .shortcut
+        height: 100%
+        overflow: hidden
+        .hot-key
+          margin: 0 20px 20px 20px
+          .title
+            margin-bottom: 20px
+            font-size: $font-size-medium
+            color: $color-text-l
+          .item
+            display: inline-block
+            padding: 5px 10px
+            margin: 0 20px 10px 0
+            border-radius: 6px
+            background: $color-highlight-background
+            font-size: $font-size-medium
+            color: $color-text-d
+        .search-history
+          position: relative
+          margin: 0 20px
+          .title
+            display: flex
+            align-items: center
+            height: 40px
+            font-size: $font-size-medium
+            color: $color-text-l
+            .text
+              flex: 1
+            .clear
+              extend-click()
+              .icon-clear
+                font-size: $font-size-medium
+                color: $color-text-d
+    .search-result
+      position: fixed
+      width: 100%
+      top: 178px
+      bottom: 0
 </style>
